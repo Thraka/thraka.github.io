9c40124455278da6ece759e050cdcbcc1ed42ef6
SadConsole <sadconsole@outlook.com>
2024-12-07 15:59:46 -0800

    Update for 10.5.0

---
+++ articles/snippets/how-to-use-imgui/csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+Builder gameStartup = new Builder()
+    .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<SCTesting.Scenes.RootScreen>()
+    .IsStartingScreenFocused(true)
+    .ConfigureFonts(true)
+    .EnableImGui(startupAction: (imguiComponent) => {
+        imguiComponent.UIComponents.Add(new SCTesting.ImGuiWindow1());
+    })
+    ;
+
+Game.Create(gameStartup);
+Game.Instance.Run();
+Game.Instance.Dispose();
